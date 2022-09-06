2820cb443a97fa46e9074c3ca55d78dd377ae30f
Amethyst Reese <amy@n7.gg>
2022-09-05 23:29:08 -0700

    Don't regenerate targets when setting up bundled libs

--- noxfile.py
+++ noxfile.py
@@ -80,6 +80,7 @@ def _check_files(names: List[str]) -> None:
 
 
 def _update_pip_packages(session: nox.Session) -> None:
+    session.install("wheel", "pip-tools")
     session.run(
         "pip-compile",
         "--generate-hashes",
@@ -140,18 +141,18 @@ def _update_npm_packages(session: nox.Session) -> None:
 
 def _setup_template_environment(session: nox.Session) -> None:
     session.install("wheel", "pip-tools")
-    session.run(
-        "pip-compile",
-        "--generate-hashes",
-        "--upgrade",
-        "./requirements.in",
-    )
-    session.run(
-        "pip-compile",
-        "--generate-hashes",
-        "--upgrade",
-        "./src/test/python_tests/requirements.in",
-    )
+    # session.run(
+    #     "pip-compile",
+    #     "--generate-hashes",
+    #     "--upgrade",
+    #     "./requirements.in",
+    # )
+    # session.run(
+    #     "pip-compile",
+    #     "--generate-hashes",
+    #     "--upgrade",
+    #     "./src/test/python_tests/requirements.in",
+    # )
     _install_bundle(session)
 
 
